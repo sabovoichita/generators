266b4d9af7e2cd15c6566f3911cc1605d2fb3109
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 19:34:37 +0100

    16. Add groups v.1

--- circle-slices.js
+++ circle-slices.js
@@ -1,51 +1,152 @@
-console.log("Welcome");
 // TODO check this examples:
 //   https://stackoverflow.com/questions/10028182/how-to-make-a-pie-chart-in-css
-// let phrases = new Array(6).fill("Text").map((text, index) => `${text} ${index + 1}`);
-let phrases = [
-  // 1 = Darul de a rezolva probleme
-  "Discernământ spiritual interior",
-  "Creativitate si inventivitate",
-  "Luarea deciziilor",
-  // 2 = Daruri de pastor-invatator
-  "Consiliind",
-  "Pregatind si conducand inchinarea",
-  "Facand ucenici",
-  "Predicand",
-  "Zidind unitatea trupului",
-  // 3 = Orientat spre crestere
-  "Capabil pentru viziune",
-  "Adaptaptindu-se la crestere",
-  "Deschis la crestere",
-  "Motivatii personale",
-  // 4 = Penetrare in societate
-  "Stil flexibil",
-  "Activitate evang. in/afara Bisericii",
-  "Prezenta in Biserica",
-  "Raspunde nevoilor Comunitatii",
-  // 5 = Abilitatile conducerii
-  "Autoritate si Influenta",
-  "Dezvoltarea conducerii Bisericii",
-  "Motivarea Membrilor",
-  "Utilizarea darurilor",
-  "Formand o lucrare care se mentine"
-];
-phrases = [...phrases, ...phrases]; // TMP duplicate phrases
-const length = phrases.length;
-const radius = 360 / length;
-const odd = length % 2 === 1 ? 0 : 1;
-document.querySelector(".circle").innerHTML =
-  phrases
-    .map((phrase, index) => {
-      const angle = Math.round(index * radius);
-      const color = `hsl(${Math.round(index * radius)}, 100%, 50%)`;
-      return `
-                  <div class="slice-line" style="--angle: ${angle}deg; --color: ${color}"></div>
-                  <div class="slice-text" style="--angle: ${
-                    angle + odd * Math.round(radius / 2)
-                  }deg; --color: ${color}">
-                      <div class="phrase-inner" style="z-index: ${phrases.length + index}">${index}.${phrase}</div>
-                  </div>
-                `;
-    })
-    .join("") + `<div class="inner-circle"></div>`;
+
+let phrases = `
+  #1 Darul de a rezolva probleme
+  Discernământ spiritual interior
+  Creativitate si inventivitate
+  Luarea deciziilor
+  
+  #2 Daruri de pastor-invatator
+  Consiliind
+  Pregatind si conducand inchinarea
+  Facand ucenici
+  Predicand
+  Zidind unitatea trupului
+  
+  #3 Orientat spre crestere
+  Capabil pentru viziune
+  Adaptaptindu-se la crestere
+  Deschis la crestere
+  Motivatii personale
+  
+  #4 Penetrare in societate
+  Stil flexibil
+  Activitate evang. in/afara Bisericii
+  Prezenta in Biserica
+  Raspunde nevoilor Comunitatii
+  
+  #5 Abilitatile conducerii
+  Autoritate si Influenta
+  Dezvoltarea conducerii Bisericii
+  Motivarea Membrilor
+  Utilizarea darurilor
+  Formand o lucrare care se mentine
+  
+  #6 Abilitati administrative
+  Primind/oferind informatii si raport
+  Administrarea timpului
+  Planificarea obiectivelor. Adaptarea / corectarea lor
+  Organizarea structurilor administrative
+  
+  #7 Cunostinte teologice / tehnice
+  Abilitati financiare si contabile
+  Reguli si legi guvernamentale
+  Pregatirea lucrarii si confirmarea ei
+  Practica si politica denominationala
+  Principiile Cesterii Bsericilor
+  
+  #8 Abilitati interpersonale si de comunicare
+  Ascultarea
+  Interactiuni
+  Mentinerea relatiilor
+  Probleme de constructii
+  Rezolvarea conflictelor
+  
+  #9 Stabilitate personala
+  Facand fata stresului
+  Auto-incredere si auto-apreciere
+  Maturitate emotionala
+  Flexibilitate    
+  
+  #10 Stabilitate in familie
+  Rol si responsabilitate de soti
+  Rol si responsabilitate de sot
+  Cooperare in familie
+`;
+
+// phrases = `
+//   #1 Darul de a rezolva probleme
+//   Discernământ spiritual interior
+//
+//   #2 Daruri de pastor-invatator
+//   Consiliind
+//   Pregatind si conducand inchinarea - Facand ucenici
+//
+//   #3 Orientat spre crestere
+//   Capabil pentru viziune
+// `;
+
+phrases = phrases
+  .split("\n")
+  .map(line => line.trim())
+  .filter(line => line.length > 0);
+
+//const titles = phrases.filter(line => line.startsWith("#"));
+// group phrases by titles and remove titles from phrases (return title + phrases.length after them)
+
+const titles = phrases.reduce((acc, line) => {
+  if (line.startsWith("#")) {
+    acc.push({
+      text: line.substring(1).trim(),
+      children: []
+    });
+  } else {
+    acc[acc.length - 1].children.push(line);
+  }
+  return acc;
+}, []);
+
+phrases = phrases
+  .filter(line => !line.startsWith("#"))
+  .map(line => ({
+    text: line
+  }));
+
+function createObjects(phrases) {
+  const length = phrases.reduce((acc, item) => acc + (item.children || [1]).length, 0);
+  const radius = 360 / length;
+  const odd = length % 2 === 1 ? 0 : 1;
+  let index = 0;
+  return phrases.map(({ text, children = [] }, i) => {
+    const slices = children.length || 1;
+    index += slices;
+    const elementAngle = Math.round(slices * radius);
+    const angle = Math.round(index * radius);
+    const color = `hsl(${Math.round(index * radius)}, 100%, 50%)`;
+    const lineAngle = angle + odd * Math.round(radius / 2);
+    const textAngle = angle - elementAngle / 2 + odd * Math.round(radius / 2);
+
+    return {
+      line: `<div class="slice-line" style="--angle: ${lineAngle}deg; --color: ${color}"></div>`,
+      text: `<div class="slice-text" style="--angle: ${textAngle}deg; --color: ${color}">
+          <div class="phrase-inner">${text}</div>
+        </div>`
+    };
+  });
+}
+
+function createSlices(circle, phrases, width = 800, innerWidth = 250) {
+  circle.style.width = `${width}px`;
+  circle.style.height = `${width}px`;
+  circle.style.setProperty("--line-width", `${(width - innerWidth) / 2}px`);
+  // circle.style.setProperty("--padding-start", `${innerWidth + 10}px`);
+  circle.style.setProperty("--padding-start", `${innerWidth}px`);
+  circle.style.setProperty("--inner-width", `${innerWidth}px`);
+
+  const objects = createObjects(phrases);
+  circle.innerHTML = objects.map(({ line }) => line).join("");
+  circle.innerHTML += objects.map(({ text }) => text).join("");
+}
+
+const groups = document.querySelector("#groups");
+createSlices(groups, titles, 1000, 800);
+groups.innerHTML += `<div id="slices" class="circle"></div>`;
+
+const slices = document.querySelector("#slices");
+createSlices(slices, phrases, 800, 250);
+slices.innerHTML += `<div class="inner-circle"></div>`;
+// TMP
+// slices.style.opacity = 0.5;
+
+console.debug(titles);
