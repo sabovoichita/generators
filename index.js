6347bec7d28f24580bb21b3bfc205cd908e47936
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 17:05:06 +0100

    7. Function decreaseFont

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function displayNames(names) {
   Array(9 - (names.length % 9))
     .fill(0)
     .forEach(() => names.push(" "));
+
   const badges = names.map((name) => {
     const parts = name.split(/\s+/);
     console.log("printing");
@@ -31,6 +32,28 @@ function displayNames(names) {
   });
   document.querySelector("#badges").innerHTML = badges.join("");
 }
+
+function decreaseFont() {
+  const defaultFontSize = 36;
+
+  let changed = false;
+  document.querySelectorAll(".badge").forEach((badge) => {
+    if (badge.offsetWidth < badge.scrollWidth) {
+      changed = true;
+      const elements = Array.from(badge.querySelectorAll("div"));
+      elements.sort((a, b) => b.offsetWidth - a.offsetWidth);
+      const [first] = elements;
+      const fontSize = parseInt(first.style.fontSize || defaultFontSize) - 1;
+      first.style.fontSize = `${fontSize}px`;
+    }
+  });
+  return changed;
+}
+
+let changed = false;
+do {
+  changed = decreaseFont();
+} while (changed);
 
 const names = getStoredNames();
 document.querySelector("#names").value = names.join("\n");
