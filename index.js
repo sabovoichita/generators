225b4b8533e44ab70ec12686156340ecfe5adec8
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 16:56:26 +0100

    5. design and print 1 badge, changed logo

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ function initEvents() {
       .split("\n")
       .map((name) => name.trim())
       .filter((name) => name);
-    console.log("names input: ", names);
+    // console.log("names input: ", names);
 
     storeNames(names);
+    displayNames(names);
   });
+}
+function displayNames(names) {
+  names = [...names];
+  const badges = names.map((name) => {
+    const parts = name.split(/\s+/);
+    console.log("printing");
+    return `<div class="badge">
+    ${parts.map((part) => `<div>${part}</div>`).join("")}
+  </div>`;
+  });
+  document.querySelector("#badges").innerHTML = badges.join("");
 }
 
 const names = getStoredNames();
 document.querySelector("#names").value = names.join("\n");
-
+displayNames(names);
 initEvents();
