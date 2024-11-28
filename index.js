d560919a655ae3b5a9fbdcf90f324cd340a0c6cb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 16:33:08 +0100

    4. Function storeNames and getStoredNames

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+function storeNames(names) {
+  localStorage.setItem("badgesNames", JSON.stringify(names));
+}
+function getStoredNames() {
+  return JSON.parse(localStorage.getItem("badgeNames")) || [];
+}
+
 function initEvents() {
   document.querySelector("#names").addEventListener("input", function () {
     const names = this.value
       .split("\n")
       .map((name) => name.trim())
       .filter((name) => name);
-    console.log("names: ", names);
+    console.log("names input: ", names);
+
+    storeNames(names);
   });
 }
+
+const names = getStoredNames();
+document.querySelector("#names").value = names.join("\n");
+
 initEvents();
