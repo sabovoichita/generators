4021d890206613707f89c6a04b93cf9fc852d622
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 17:51:53 +0100

    9. Add portrait and landscape modes

--- index.js
+++ index.js
@@ -1,61 +1,81 @@
-function storeNames(names) {
-  localStorage.setItem("badgesNames", JSON.stringify(names));
+let portrait = document.body.classList.contains("portrait");
+
+function setOrientation(portraitValue) {
+  portrait = portraitValue;
+  document.body.classList.remove(portraitValue ? "landscape" : "portrait");
+  document.body.classList.add(portraitValue ? "portrait" : "landscape");
+  displayNames(getStoredNames(), portraitValue);
 }
+
 function getStoredNames() {
   return JSON.parse(localStorage.getItem("badgeNames")) || [];
+}
+
+function storeNames(names) {
+  localStorage.setItem("badgeNames", JSON.stringify(names));
 }
 
 function initEvents() {
   document.querySelector("#names").addEventListener("input", function () {
     const names = this.value
       .split("\n")
-      .map((name) => name.trim())
-      .filter((name) => name);
-    // console.log("names input: ", names);
-
+      .map(name => name.trim())
+      .filter(name => name);
     storeNames(names);
-    displayNames(names);
+    displayNames(names, portrait);
   });
 }
-function displayNames(names) {
-  names = [...names];
-  Array(9 - (names.length % 9))
-    .fill(0)
-    .forEach(() => names.push(" "));
 
-  const badges = names.map((name) => {
+function displayNames(names, portrait = true) {
+  names = [...names];
+  const itemsPerPage = portrait ? 9 : 5; // 5 because will duplicate each name
+
+  if (names.length % itemsPerPage !== 0) {
+    new Array(itemsPerPage - (names.length % itemsPerPage)).fill(0).forEach(() => names.push(" "));
+  }
+
+  if (!portrait) {
+    //TODO duplicate each row to make it double sided (one row has 5 elements
+    names = names.reduce((acc, name, index, all) => {
+      acc.push(name);
+      if (index % 5 === 4) {
+        acc.push(...all.slice(index - 4, index + 1));
+      }
+      return acc;
+    }, []);
+  }
+
+  const defaultFontSize = 36;
+  const badges = names.map((name, i) => {
     const parts = name.split(/\s+/);
-    console.log("printing");
-    return `<div class="badge">
-    ${parts.map((part) => `<div>${part}</div>`).join("")}
-  </div>`;
+    const flip = portrait ? "" : i % 10 >= 5 ? "flip" : "";
+    return `<div class="badge ${flip}">
+          ${parts.map(part => `<div>${part}</div>`).join("")}
+        </div>`;
   });
   document.querySelector("#badges").innerHTML = badges.join("");
-}
 
-function decreaseFont() {
-  const defaultFontSize = 36;
-
+  function decreaseFont() {
+    let changed = false;
+    document.querySelectorAll(".badge").forEach(badge => {
+      if (badge.offsetWidth < badge.scrollWidth) {
+        changed = true;
+        const elements = Array.from(badge.querySelectorAll("div"));
+        elements.sort((a, b) => b.offsetWidth - a.offsetWidth);
+        const [first] = elements;
+        const fontSize = parseInt(first.style.fontSize || defaultFontSize) - 1;
+        first.style.fontSize = `${fontSize}px`;
+      }
+    });
+    return changed;
+  }
   let changed = false;
-  document.querySelectorAll(".badge").forEach((badge) => {
-    if (badge.offsetWidth < badge.scrollWidth) {
-      changed = true;
-      const elements = Array.from(badge.querySelectorAll("div"));
-      elements.sort((a, b) => b.offsetWidth - a.offsetWidth);
-      const [first] = elements;
-      const fontSize = parseInt(first.style.fontSize || defaultFontSize) - 1;
-      first.style.fontSize = `${fontSize}px`;
-    }
-  });
-  return changed;
+  do {
+    changed = decreaseFont();
+  } while (changed);
 }
-
-let changed = false;
-do {
-  changed = decreaseFont();
-} while (changed);
 
 const names = getStoredNames();
 document.querySelector("#names").value = names.join("\n");
-displayNames(names);
+displayNames(names, portrait);
 initEvents();
